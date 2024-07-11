668f96420d6b1d3c7ecff90cb5c218897c1a69f3
K11BN367 <alexander592beckmann@gmail.com>
2024-07-11 20:29:36 +0200

    initial commit

---
+++ src/SoftRandom.jl
@@ -0,0 +1,10 @@
+module RandomWrapper
+    using SoftBase    
+    import Random
+    abstract type c__Default_Random end
+    function c__Default_Random()
+        return Random.default_rng()
+    end
+    const u__Random = u{Random.AbstractRNG}
+    export u__Random
+end
