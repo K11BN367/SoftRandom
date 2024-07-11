e6b9ebb8001b97623531c69ea5cefe69cfd91f4e
K11BN367 <alexander592beckmann@gmail.com>
2024-07-11 23:48:34 +0200

    Add export statement for c__Default_Random function in SoftRandom.jl

--- src/SoftRandom.jl
+++ src/SoftRandom.jl
@@ -5,6 +5,8 @@ module SoftRandom
     function c__Default_Random()
         return Random.default_rng()
     end
+    export c__Default_Random
+    const Default_Random = c__Default_Random()
     const u__Random = u{Random.AbstractRNG}
     export u__Random
 end
