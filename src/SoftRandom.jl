43e166c0659f5d9bb3959a7669de97a183dc7c2a
K11BN367 <alexander592beckmann@gmail.com>
2024-07-26 13:11:32 +0200

    commit

--- src/SoftRandom.jl
+++ src/SoftRandom.jl
@@ -9,4 +9,6 @@ module SoftRandom
     const Default_Random = c__Default_Random()
     const u__Random = u{Random.AbstractRNG}
     export u__Random
+    const shuffle = Random.shuffle
+    export shuffle
 end
